0a1793ba32ec4a7111acaf08364c63daefe9610e
Nolan <nolanbeguhl@gmail.com>
2024-07-24 16:15:53 -0700

    fix action notification

--- src/components/deleteItem.js
+++ src/components/deleteItem.js
@@ -1,7 +1,7 @@
 import { addAction } from "./notifications.js";
 
 function deleteItem(project, item) {
-    addAction(`${item.title} deleted from ${project.name}.`);
+    addAction(`${item.title} task deleted from ${project.name} project.`);
     project.removeItem(item);
 }
 
